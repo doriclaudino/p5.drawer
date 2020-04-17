697659979bf949dff5c2cd4509bd2bbbfe57cd49
dori <dori.claudino@gmail.com>
2020-04-17 16:57:42 -0400

    mocha report

--- test/test.js
+++ test/test.js
@@ -9,6 +9,20 @@ require.config({
 
 var allTests = ['tests/p5.Drawer'];
 
+after(function (done) {
+  var total = 0;
+  var passed = 0;
+  var failed = 0;
+  console.log(mocha)
+  mocha.suite.suites
+  .forEach((suite) => {
+    passed += suite.tests.filter((test) => test.state === 'passed').length;
+    total += suite.tests.length;
+    failed += total - passed;
+  });
+  console.log({ total, passed, failed });
+  done();
+});
 require(allTests, function () {
   mocha.run();
 });
