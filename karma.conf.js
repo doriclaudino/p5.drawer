f0b5e0e0a24990c2fd1e71d3c02a069ed3f1ae6f
dori <dori.claudino@gmail.com>
2020-04-18 04:34:52 -0400

    display and  ChromeHeadlessNoSandbox

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,13 @@ module.exports = function (config) {
     port: 9876, // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     autoWatch: false,
     // singleRun: false, // Karma captures browsers, runs the tests and exits
     concurrency: Infinity,
